1278cfe64ebbb9e4002f769daa7182e7c23d1528
rishi <rishichawda85@gmail.com>
2018-03-14 13:56:08 +0000

    font awesome added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,21 @@
 
 @section('content')
 <div class="container">
-    <table>
-        <tr>
-            
+    
+    <table class="table">
+            <thead class="thead-dark">
+                    <tr>
+                      <th scope="col"></th>
+                      <th scope="col"></th>
+                    </tr>
+                  </thead>
+        <tbbody>
+        <tr>               
+           <td> <i class="fa fa-address-book fa-4x" aria-hidden="true"></i></td>
+           <td> <i class="fa fa-arrows-h fa-4x" aria-hidden="true"></i></td>
         </tr>
-
+        </tbbody>
     </table>
         
     </div>
-</div>
 @endsection
