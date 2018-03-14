d150e1ac846265578522f07b07972dfed6d3d0ab
rishi <rishichawda85@gmail.com>
2018-03-14 14:48:19 +0000

    dashaboard added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,16 @@
 
 @section('content')
 <div class="container">
-    
-    <table class="table">
-            <thead class="thead-dark">
-                    <tr>
-                      <th scope="col"></th>
-                      <th scope="col"></th>
-                    </tr>
-                  </thead>
-        <tbbody>
-        <tr>               
-           <td> <i class="fa fa-address-book fa-4x" aria-hidden="true"></i></td>
-           <td> <i class="fa fa-arrows-h fa-4x" aria-hidden="true"></i></td>
-        </tr>
-        </tbbody>
-    </table>
-        
-    </div>
+        <div class="row justify-content-md-center">
+        <h2>Dashboard</h2>
+        </div>    
+        <div class="row">
+                <div class="col-md-auto">
+                    <i class="fa fa-address-book fa-4x" aria-hidden="true"></i>
+                </div>
+                <div class="col-md-auto">
+                        <i class="fa fa-arrows-h fa-4x" aria-hidden="true"></i>
+                </div>            
+              </div>
+            </div>
 @endsection
